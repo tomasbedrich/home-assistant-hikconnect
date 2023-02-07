b9ae5e32864053fb05ac9c7c4da15fbf17dc251f
Tomas Bedrich <ja@tbedrich.cz>
2023-02-07 11:22:41 +0100

    Upgrade to async_forward_entry_setups
    See: https://developers.home-assistant.io/blog/2022/07/08/config_entry_forwards/

--- custom_components/hikconnect/__init__.py
+++ custom_components/hikconnect/__init__.py
@@ -92,7 +92,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
         "api": api,
         "coordinator": coordinator,
     }
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
 
